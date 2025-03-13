43b6a70abd477d8c0c8c165a68800ecc79eadb96
Varun Penumudi <73636316+varunpenumudi@users.noreply.github.com>
2025-03-13 18:50:22 +0530

    The hello Poem
    created a poem to say hello to user

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello Freind!")
+hello_string = """
+Hello there, friend, it's great you're here,
+A warm welcome wrapped in cheer!
+Let's dive in deep, explore, create,
+With ideas bright — let's innovate.
+"""
+
+
+print(hello_string)
