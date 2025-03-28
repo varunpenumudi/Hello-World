e35da36ec636cfd2a907a68f597046ee852db411
Varun Penumudi <73636316+varunpenumudi@users.noreply.github.com>
2025-03-28 12:05:19 +0530

    printing the username in poem
    printing the username taken previously as input in the poem

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 name = input("Enter name:")
 
-hello_string = """
-Hello there, friend, it's great you're here,
+hello_string = f"""
+Hello there, {name}, it's great you're here,
 A warm welcome wrapped in cheer!
 Let's dive in deep, explore, create,
 With ideas bright — let's innovate.
