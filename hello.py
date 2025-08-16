11648973ab08d722ce1e568b9f03dd3df7744036
Varun Penumudi <73636316+varunpenumudi@users.noreply.github.com>
2025-08-16 11:53:38 +0530

    Added Emojies
    Adding Emojies as referred in #4

--- hello.py
+++ hello.py
@@ -2,10 +2,10 @@ name = input("Enter name:")
 
 hello_string = f"""
 
-Hello there, {name}, it's great you're here,
-A warm welcome wrapped in cheer!
+👋Hello there, {name}, it's great you're here,
+A warm welcome wrapped in cheer🥂!
 Let's dive in deep, explore, create,
-With ideas bright — let's innovate.
+With ideas bright — let's innovate😇.
 
 """
 
